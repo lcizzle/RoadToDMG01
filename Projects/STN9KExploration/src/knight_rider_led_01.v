32d96a51cbce8fc0880ca2269951b9d101ecc12d
lcizzle <lcizzle@lcizzleshizzle.com>
2025-08-28 22:27:33 +0900

    Knight Rider LED 01 Design

---
+++ Projects/STN9KExploration/src/knight_rider_led_01.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 10ps               // Timescale for simulation?
+
+module knight_rider_led_01          // Module declaration
+    (
+        input wire          clk,    // Clock input
+        output reg [5:0]    led     // 6 LED outputs
+    );
+
+    parameter CLOCK_DIV = 8;        // Clock divider parameter
+
+    // The board has a 27Mhz oscillator chip on pin 52
+    // Don't forget to set the timing constraints and assign a clock divider
+    // Counter that counts to 27,000,000
+    reg [31:0] count = 0;           // 32-bit counter
+    reg dir = 0;                    // Direction flag
+    reg [5:0] pattern = 6'b000001;  // LED pattern
+
+    // Always block
+    always @(posedge clk) begin
+        if (count == 27_000_000 / CLOCK_DIV - 1) begin
+            count <= 0;             // Reset counter
+            
+            // Reverse direction before shifting
+            if(!dir) begin
+                if(pattern == 6'b100000) begin
+                    dir <= 1;
+                end else begin
+                    pattern <= pattern << 1;
+                end 
+            end else begin
+                if(pattern == 6'b000001) begin 
+                    dir <= 0;
+                end else begin
+                    pattern <= pattern >> 1;
+                end
+            end
+        end else begin
+            count <= count + 1;     // Increment counter
+        end
+    end
+
+    always @(*) begin
+        led = ~pattern;             // Active low LEDs
+    end
+
+endmodule                           // End of module
+    
