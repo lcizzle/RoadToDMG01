e6567ba853f79fa10c5b98b10e6ff7176046ba7f
lcizzle <lcizzle@lcizzleshizzle.com>
2025-08-29 07:09:01 +0900

    Moved and Renamed Enable LED 01 Design

--- Projects/STN9KExploration/src/enable_led_01/enable_led_01_top.v
+++ Projects/STN9KExploration/src/enable_led_01/enable_led_01_top.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 10ps               // Timescale for simulation?
 
-module enable_led_01                // Module declaration
+module enable_led_01_top            // Module declaration
     (
         output wire [5:0]    led    // 6 LED outputs
     );
