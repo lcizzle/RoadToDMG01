a2f063d43c1c9aeb5288ae5e8d5542e2694e136d
lcizzle <lcizzle@lcizzleshizzle.com>
2025-08-31 01:54:17 +0900

    Added Module AND Gate 01 and Module BUFFER Gate 01

---
+++ Projects/STN9KExploration/src/modules/m_and_gate_01.v
@@ -0,0 +1,12 @@
+`timescale 1ns / 10ps
+
+module m_and_gate_01
+    (
+        input      A,
+        input      B,
+        output     X
+    );
+
+    assign X = A & B;
+
+endmodule
---
+++ Projects/STN9KExploration/src/modules/m_buffer_gate_01.v
@@ -0,0 +1,10 @@
+`timescale 1ns / 10ps
+
+module m_buffer_gate_01
+    (
+        input      A,
+        output     B
+    );
+
+    assign B = A;
+endmodule
