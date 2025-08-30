9f2a82d0b8d9362583cce1df112c7c9b408319db
lcizzle <lcizzle@lcizzleshizzle.com>
2025-08-30 18:49:39 +0900

    Clean up for consistency

--- Projects/STN9KExploration/src/blink_led_01/blink_led_01_top.v
+++ Projects/STN9KExploration/src/blink_led_01/blink_led_01_top.v
@@ -2,22 +2,22 @@
 
 module blink_led_01_top             // Module declaration
     (
-        input wire          clk,    // Clock input
+        input wire          clk_in, // Clock input
         output reg [5:0]    led     // 6 LED outputs
     );
 
     // The board has a 27Mhz oscillator chip on pin 52
     // Don't forget to set the timing constraints and assign a clock
     // Counter that counts to 27,000,000
-    reg [24:0] count = 0;
+    reg [24:0] counter = 0;
 
     // Always block
-    always @(posedge clk) begin
-        if (count == 27_000_000 - 1) begin
-            count <= 0;         // Reset counter
-            led <= ~led;        // Toggle LEDs
+    always @(posedge clk_in) begin
+        if (counter == 27_000_000 - 1) begin
+            counter <= 0;           // Reset counter
+            led <= ~led;            // Toggle LEDs
         end else begin
-            count <= count + 1; // Increment counter
+            counter <= counter + 1'd1; // Increment counter
         end
     end
 
--- Projects/STN9KExploration/src/enable_led_01/enable_led_01_top.v
+++ Projects/STN9KExploration/src/enable_led_01/enable_led_01_top.v
@@ -5,6 +5,7 @@ module enable_led_01_top            // Module declaration
         output wire [5:0]    led    // 6 LED outputs
     );
 
+    // LED's are active LOW
     //assign led = 6'b111111;       // Turn off all LEDs
     assign led = 6'b000000;         // Turn on all LEDs
 
