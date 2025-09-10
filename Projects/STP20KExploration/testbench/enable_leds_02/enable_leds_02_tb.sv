9bfed7a287b55984f8de9d68be5e7810889a1253
lcizzle <lcizzle@lcizzleshizzle.com>
2025-09-10 20:43:42 +0900

    Updated Enable LEDs 02 TB

--- Projects/STP20KExploration/testbench/enable_leds_02/enable_leds_02_tb.sv
+++ Projects/STP20KExploration/testbench/enable_leds_02/enable_leds_02_tb.sv
@@ -13,42 +13,47 @@ enable_leds_02 dut(.*);
     initial begin 
         $printtimescale(enable_leds_02_tb);
 
-        for(int i = 0; i < 4; i++) begin 
-            $display("Setting buttons to %2b", i[1:0]); 
-            btns_n = i[1:0]; 
-            #100; 
-        end 
-        
-        $display("PASS: Logic test passed."); 
-        $stop; 
-    end 
-    
-    //
-    always @(btns_n) begin
-        logic btn_0, btn_1;
-        logic [3:0] expected_leds;
-        logic [3:0] actual_leds;
+        // Default button state
+        btns_n = '1;
 
-        #5;
+        for(int i = 0; i < 4; i++) begin
+            logic btn_0, btn_1;
+            logic [3:0] expected_leds;
+            logic [3:0] actual_leds;
 
-        btn_0 = ~btns_n[0];
-        btn_1 = ~btns_n[1];
+            $display("Setting buttons %2b", i[1:0]);
+            btns_n = i[1:0];
 
-        expected_leds[0] = ~btn_0;
-        expected_leds[1] = btn_1 && btn_0;
-        expected_leds[2] = btn_1 || btn_0;
-        expected_leds[3] = btn_1  ^ btn_0;
+            #5; // Wait for combinational DUT logic to update.
 
-        actual_leds = ~leds_n;
+            //
+            btn_0 = ~btns_n[0];
+            btn_1 = ~btns_n[1];
 
-        $display("Time:%0tns stim(btns_n):%b -> dut(leds_n):%b -> actual:%b, expected:%b", $time, btns_n, leds_n, actual_leds, expected_leds);
+            //
+            expected_leds[0] = !btn_0;
+            expected_leds[1] =  btn_1 && btn_0;
+            expected_leds[2] =  btn_1 || btn_0;
+            expected_leds[3] =  btn_1 ^  btn_0;
 
-        if(actual_leds !== expected_leds) begin
-            $error("FAIL: Mismatch");
-            $display("Expected leds: %4b", expected_leds);
-            $display("Actual   leds: %4b", actual_leds);
-            $stop;
+            //
+            actual_leds = ~leds_n;
+
+            //
+            $display("Time:%0tns stim(btns_n):%b -> DUT(leds_n):%b -> actual:%b, expected:%b", $time, btns_n, leds_n, actual_leds, expected_leds);
+
+            if(actual_leds !== expected_leds) begin
+                $error("FAIL: LEDs mismtach detected!");
+                $display(" Expected LEDs: %4b", expected_leds);
+                $display(" Actual   LEDs: %4b", actual_leds);
+                $stop;
+            end
+
+            #100;
         end
+
+        $display("PASS: Logic checks passed.");
+        $stop;
     end
 
 endmodule
